f6b974c3c591c472c19504ad2837a9cb59000cb6
TheFlightSimsOfficial <theflightsimulations@gmail.com>
2023-05-27 10:30:42 +0700

    Redesign the lab env

--- admin-scripts/jupyter_enable_ibmq.sh
+++ admin-scripts/jupyter_enable_ibmq.sh
@@ -15,6 +15,8 @@ if [[ "${CONFIRM}" =~ ^[Yy]$ ]]; then
 	sudo cp -r ~/admin-scripts/jupyter-external-packages/ibm-q-lab/etc /usr
 	sudo cp -r ~/admin-scripts/jupyter-external-packages/ibm-q-lab/local /usr
 	sudo cp -r ~/admin-scripts/jupyter-external-packages/ibm-q-lab/share /usr
+	sudo pip install openqasm3 docplex  h5py  fastdtw scikit-learn fastdtw nasdaq-data-link yfinance lmfit uncertainties terminado 'dill==0.3.5' 'jupyter-server>=2.0.1' 'jupyterlab==3.6.1' 'urllib3==1.26.16' 'PyJWT>=2.4.0' 'voila==0.5.0a4'
+	sudo pip uninstall -y jupyterlab-genv
 	echo -e "Vailidating pip installations..."
     sudo pip check
 else
