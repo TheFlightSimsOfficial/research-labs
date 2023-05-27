1bacf6e0a8a27b4ee02153953ead3af919b2cd18
TheFlightSimsOfficial <theflightsimulations@gmail.com>
2023-05-27 12:47:07 +0700

    Implement IBM-Q into default system

--- admin-scripts/jupyter_setup_server.sh
+++ admin-scripts/jupyter_setup_server.sh
@@ -247,6 +247,18 @@ if [[ "${CONFIRM}" =~ ^[Yy]$ ]]; then
 	sudo cp -TRv ~/admin-scripts/tutorials-notebooks /etc/jupyter/tutorials-notebooks
 	sudo chmod +rwxrxrx /etc/jupyter/tutorials-notebooks
 
+	# Enable IBM-Q
+	echo -e "Copying IBM-Q, Qiskit and its dependencies..."
+	sudo cp -r ~/admin-scripts/jupyter-external-packages/ibm-q-lab/bin /usr
+	sudo cp -r ~/admin-scripts/jupyter-external-packages/ibm-q-lab/etc /usr
+	sudo cp -r ~/admin-scripts/jupyter-external-packages/ibm-q-lab/local /usr
+	sudo cp -r ~/admin-scripts/jupyter-external-packages/ibm-q-lab/share /usr
+	sudo pip install openqasm3 docplex  h5py  fastdtw scikit-learn fastdtw nasdaq-data-link yfinance lmfit uncertainties terminado 'dill==0.3.5' 'jupyter-server>=2.0.1' 'jupyterlab==3.6.1' 'urllib3==1.26.16' 'PyJWT>=2.4.0' 'voila==0.5.0a4'
+	sudo pip uninstall -y jupyterlab-genv
+	echo -e "Vailidating pip installations..."
+    sudo pip check
+	
+	
 	echo -e "\nInstalled Jupyter Lab for multiple users!"
 else
 	echo "Aborted!"
