6a74188a823385d1248749df1bd3be328aa3b654
TheFlightSimsOfficial <theflightsims@gmail.com>
2023-06-29 20:18:32 +0700

    Update PIP Package test

--- admin-scripts/jupyter_setup_server.sh
+++ admin-scripts/jupyter_setup_server.sh
@@ -71,150 +71,150 @@ if [[ "${CONFIRM}" =~ ^[Yy]$ ]]; then
     sudo pip check
 
 	# Sudo install other PIP packages from database
-	pip install 'altair==5.0.1'
-	pip install 'aquirdturtle-collapsible-headings==3.1.0'
-	pip install 'async-lru==2.0.2'
-	pip install 'autopep8==2.0.2'
-	pip install 'azure-quantum==0.28.277227'
-	pip install 'backports.functools-lru-cache==1.6.4'
-	pip install 'blinker==1.4'
-	pip install 'bokeh==3.1.1'
-	pip install 'bottleneck==1.3.7'
-	pip install 'brotlipy==0.7.0'
-	pip install 'cached-property==1.5.2'
-	pip install 'clear==2.0.0'
-	pip install 'cliapp==1.20180812.1'
-	pip install 'cmdtest==0.32+git'
-	pip install 'command-not-found==0.3'
-	pip install 'conan==2.0.6'
-	pip install 'cvxpy==1.3.1'
-	pip install 'cytoolz==0.12.1'
-	pip install 'dbus-python==1.2.18'
-	pip install 'distributed==2023.5.1'
-	pip install 'distro-info==1.0'
-	pip install 'flit-core==3.9.0'
-	pip install 'fqdn==1.5.1'
-	pip install 'gaqqie-door==0.2.0'
-	pip install 'gmpy2==2.1.5'
-	pip install 'graphviz==0.20.1'
-	pip install 'heapdict==1.0.1'
-	pip install 'ibm-q-lab-server-extension==4.0.26'
-	pip install 'ibm-q-lab-ui-extensions==4.0.26'
-	pip install 'ibm-quantum-widgets==4.0.26'
-	pip install 'ibmq-jupyter-server-health-ext==0.0.1'
-	pip install 'imagecodecs==2023.3.16'
-	pip install 'importlib-resources==5.12.0'
-	pip install 'ipympl==0.9.3'
-	pip install 'ipysheet==0.7.0'
-	pip install 'isoduration==20.11.0'
-	pip install 'jlab-enhanced-cell-toolbar==3.5.1'
-	pip install 'jsonpointer==2.3'
-	pip install 'jupyter-archive==3.3.4'
-	pip install 'jupyter-contrib-nbextensions==0.7.0'
-	pip install 'jupyter-resource-usage==0.7.2'
-	pip install 'jupyter-server-terminals==0.4.4'
-	pip install 'jupyterhub-nativeauthenticator==1.2.0'
-	pip install 'jupyterlab-autoversion==0.3.6'
-	pip install 'jupyterlab-cell-flash==0.3.5'
-	pip install 'jupyterlab-code-formatter==2.2.1'
-	pip install 'jupyterlab-dash==0.1.0a3'
-	pip install 'jupyterlab-drawio==0.9.0'
-	pip install 'jupyterlab-execute-time==2.3.0'
-	pip install 'jupyterlab-fasta==3.3.0'
-	pip install 'jupyterlab-favorites==3.1.1'
-	pip install 'jupyterlab-geojson==3.4.0'
-	pip install 'jupyterlab-image-editor==0.1.0'
-	pip install 'jupyterlab-katex==3.4.0'
-	pip install 'jupyterlab-kernelspy==3.1.0'
-	pip install 'jupyterlab-language-pack-de-de==3.6.post3'
-	pip install 'jupyterlab-language-pack-fr-fr==3.6.post3'
-	pip install 'jupyterlab-language-pack-ja-jp==3.6.post3'
-	pip install 'jupyterlab-language-pack-ru-ru==3.6.post3'
-	pip install 'jupyterlab-language-pack-uk-ua==3.6.post3'
-	pip install 'jupyterlab-language-pack-vi-vn==3.6.post3'
-	pip install 'jupyterlab-limit-output==1.0.0'
-	pip install 'jupyterlab-link-share==0.3.0'
-	pip install 'jupyterlab-lsp==4.2.0'
-	pip install 'jupyterlab-markup==2.0.0'
-	pip install 'jupyterlab-mathjax3==4.3.0'
-	pip install 'jupyterlab-open-url-parameter==0.1.0'
-	pip install 'jupyterlab-pullrequests==3.0.2'
-	pip install 'jupyterlab-pyflyby==4.1.0'
-	pip install 'jupyterlab-pytutor==0.1.1'
-	pip install 'jupyterlab-recents==3.2.0'
-	pip install 'jupyterlab-skip-traceback==4.0.3'
-	pip install 'jupyterlab-spreadsheet-editor==0.6.1'
-	pip install 'jupyterlab-tour==3.1.4'
-	pip install 'jupyterlab-vega2==3.2.0'
-	pip install 'jupyterlab-vega3==3.3.0'
-	pip install 'kaleidoscope==0.2.0'
-	pip install 'keyring==23.13.1'
-	pip install 'launchpadlib==1.11.0'
-	pip install 'lckr-jupyterlab-variableinspector==3.0.9'
-	pip install 'lz4==4.3.2'
-	pip install 'mamba==0.11.2'
-	pip install 'manimlib==0.2.0'
-	pip install 'mapomatic==0.8.0'
-	pip install 'matlab==0.1'
-	pip install 'matlab-kernel==0.17.1'
-	pip install 'matlab-proxy==0.5.11'
-	pip install 'mthree==2.5.1'
-	pip install 'munkres==1.1.4'
-	pip install 'nbgitpuller==1.1.1'
-	pip install 'ndindex==1.7'
-	pip install 'netifaces==0.11.0'
-	pip install 'nxpd==0.2.0'
-	pip install 'onnxruntime==1.15.0'
-	pip install 'openpyxl==3.1.2'
-	pip install 'overrides==7.3.1'
-	pip install 'papermill==2.4.0'
-	pip install 'pip-chill==1.0.3'
-	pip install 'pkgutil-resolve-name==1.3.10'
-	pip install 'prototype-zne==1.1.0'
-	pip install 'py-spy==0.3.14'
-	pip install 'pybind11==2.10.4'
-	pip install 'pycurl==7.45.2'
-	pip install 'pydot==1.4.2'
-	pip install 'pygobject==3.44.1'
-	pip install 'pylatexenc==2.10'
-	pip install 'pymysql==1.0.3'
-	pip install 'pyscf==2.2.1'
-	pip install 'pysocks==1.7.1'
-	pip install 'pyspnego==0.9.0'
-	pip install 'python-apt==2.4.0+ubuntu1'
-	pip install 'qc-grader==0.15.3'
-	pip install 'qiskit-aqt-provider==0.4.3'
-	pip install 'qiskit-braket-provider==0.0.3'
-	pip install 'qiskit-experiments==0.4.0'
-	pip install 'qiskit-finance==0.3.4'
-	pip install 'qiskit-ibm-provider==0.5.0'
-	pip install 'qiskit-ibm-runtime==0.9.0'
-	pip install 'qiskit-ionq==0.4.0'
-	pip install 'qiskit-iqm==8.1'
-	pip install 'qiskit-machine-learning==0.6.1'
-	pip install 'qiskit-nature==0.6.0'
-	pip install 'qiskit-qasm3-import==0.2.0'
-	pip install 'qiskit-qir==0.3.1'
-	pip install 'qiskit-textbook==0.1.0'
-	pip install 'rich==13.4.1'
-	pip install 'scikit-build==0.17.6'
-	pip install 'scikit-image==0.21.0'
-	pip install 'scriptedforms==0.10.1'
-	pip install 'ssh-import-id==5.11'
-	pip install 'statsmodels==0.14.0'
-	pip install 'stickyland==0.2.1'
-	pip install 'system-info==2.0.0'
-	pip install 'systemd-python==234'
-	pip install 'tables==3.8.0'
-	pip install 'ttystatus==0.38'
-	pip install 'ubuntu-advantage-tools==8001'
-	pip install 'ufw==0.36.1'
-	pip install 'unattended-upgrades==0.1'
-	pip install 'unicodedata2==15.0.0'
-	pip install 'uri-template==1.2.0'
-	pip install 'voila==0.4.0'
-	pip install 'webcolors==1.13'
-	pip install 'xlrd==2.0.1'
+	sudo pip install altair
+	sudo pip install aquirdturtle-collapsible-headings
+	sudo pip install async-lru
+	sudo pip install autopep8
+	sudo pip install azure-quantum
+	sudo pip install backports.functools-lru-cache
+	sudo pip install blinker
+	sudo pip install bokeh
+	sudo pip install bottleneck
+	sudo pip install brotlipy
+	sudo pip install cached-property
+	sudo pip install clear
+	sudo pip install cliapp
+	sudo pip install cmdtest
+	sudo pip install command-not-found
+	sudo pip install conan
+	sudo pip install cvxpy
+	sudo pip install cytoolz
+	sudo pip install dbus-python
+	sudo pip install distributed
+	sudo pip install distro-info
+	sudo pip install flit-core
+	sudo pip install fqdn
+	sudo pip install gaqqie-door
+	sudo pip install gmpy2
+	sudo pip install graphviz
+	sudo pip install heapdict
+	sudo pip install ibm-q-lab-server-extension
+	sudo pip install ibm-q-lab-ui-extensions
+	sudo pip install ibm-quantum-widgets
+	sudo pip install ibmq-jupyter-server-health-ext
+	sudo pip install imagecodecs
+	sudo pip install importlib-resources
+	sudo pip install ipympl
+	sudo pip install ipysheet
+	sudo pip install isoduration
+	sudo pip install jlab-enhanced-cell-toolbar
+	sudo pip install jsonpointer
+	sudo pip install jupyter-archive
+	sudo pip install jupyter-contrib-nbextensions
+	sudo pip install jupyter-resource-usage
+	sudo pip install jupyter-server-terminals
+	sudo pip install jupyterhub-nativeauthenticator
+	sudo pip install jupyterlab-autoversion
+	sudo pip install jupyterlab-cell-flash
+	sudo pip install jupyterlab-code-formatter
+	sudo pip install jupyterlab-dash
+	sudo pip install jupyterlab-drawio
+	sudo pip install jupyterlab-execute-time
+	sudo pip install jupyterlab-fasta
+	sudo pip install jupyterlab-favorites
+	sudo pip install jupyterlab-geojson
+	sudo pip install jupyterlab-image-editor
+	sudo pip install jupyterlab-katex
+	sudo pip install jupyterlab-kernelspy
+	sudo pip install jupyterlab-language-pack-de-de
+	sudo pip install jupyterlab-language-pack-fr-fr
+	sudo pip install jupyterlab-language-pack-ja-jp
+	sudo pip install jupyterlab-language-pack-ru-ru
+	sudo pip install jupyterlab-language-pack-uk-ua
+	sudo pip install jupyterlab-language-pack-vi-vn
+	sudo pip install jupyterlab-limit-output
+	sudo pip install jupyterlab-link-share
+	sudo pip install jupyterlab-lsp
+	sudo pip install jupyterlab-markup
+	sudo pip install jupyterlab-mathjax3
+	sudo pip install jupyterlab-open-url-parameter
+	sudo pip install jupyterlab-pullrequests
+	sudo pip install jupyterlab-pyflyby
+	sudo pip install jupyterlab-pytutor
+	sudo pip install jupyterlab-recents
+	sudo pip install jupyterlab-skip-traceback
+	sudo pip install jupyterlab-spreadsheet-editor
+	sudo pip install jupyterlab-tour
+	sudo pip install jupyterlab-vega2
+	sudo pip install jupyterlab-vega3
+	sudo pip install kaleidoscope
+	sudo pip install keyring
+	sudo pip install launchpadlib
+	sudo pip install lckr-jupyterlab-variableinspector
+	sudo pip install lz4
+	sudo pip install mamba
+	sudo pip install manimlib
+	sudo pip install mapomatic
+	sudo pip install matlab
+	sudo pip install matlab-kernel
+	sudo pip install matlab-proxy
+	sudo pip install mthree
+	sudo pip install munkres
+	sudo pip install nbgitpuller
+	sudo pip install ndindex
+	sudo pip install netifaces
+	sudo pip install nxpd
+	sudo pip install onnxruntime
+	sudo pip install openpyxl
+	sudo pip install overrides
+	sudo pip install papermill
+	sudo pip install pip-chill
+	sudo pip install pkgutil-resolve-name
+	sudo pip install prototype-zne
+	sudo pip install py-spy
+	sudo pip install pybind11
+	sudo pip install pycurl
+	sudo pip install pydot
+	sudo pip install pygobject
+	sudo pip install pylatexenc
+	sudo pip install pymysql
+	sudo pip install pyscf
+	sudo pip install pysocks
+	sudo pip install pyspnego
+	sudo pip install python-apt
+	sudo pip install qc-grader
+	sudo pip install qiskit-aqt-provider
+	sudo pip install qiskit-braket-provider
+	sudo pip install qiskit-experiments
+	sudo pip install qiskit-finance
+	sudo pip install qiskit-ibm-provider
+	sudo pip install qiskit-ibm-runtime
+	sudo pip install qiskit-ionq
+	sudo pip install qiskit-iqm
+	sudo pip install qiskit-machine-learning
+	sudo pip install qiskit-nature
+	sudo pip install qiskit-qasm3-import
+	sudo pip install qiskit-qir
+	sudo pip install qiskit-textbook
+	sudo pip install rich
+	sudo pip install scikit-build
+	sudo pip install scikit-image
+	sudo pip install scriptedforms
+	sudo pip install ssh-import-id
+	sudo pip install statsmodels
+	sudo pip install stickyland
+	sudo pip install system-info
+	sudo pip install systemd-python
+	sudo pip install tables
+	sudo pip install ttystatus
+	sudo pip install ubuntu-advantage-tools
+	sudo pip install ufw
+	sudo pip install unattended-upgrades
+	sudo pip install unicodedata2
+	sudo pip install uri-template
+	sudo pip install voila
+	sudo pip install webcolors
+	sudo pip install xlrd
 	echo -e "Vailidating pip installations..."
     sudo pip check
 	
@@ -287,6 +287,9 @@ if [[ "${CONFIRM}" =~ ^[Yy]$ ]]; then
 	
 	# Security in folder /etc/jupyter. See issue #11
 	sudo chmod 700 /etc/jupyter
+	
+	echo -e "Cleaning up pip caches..."
+	sudo pip cache purge
 	
 	echo -e "\nInstalled Jupyter Lab for multiple users!"
 else
