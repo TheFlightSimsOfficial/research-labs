868142b581247fca6970330243ac7544a1aa3fae
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-20 21:33:12 +0700

    Update mysql_change_password_manual.sh

--- admin-scripts/mysql_change_password_manual.sh
+++ admin-scripts/mysql_change_password_manual.sh
@@ -8,15 +8,15 @@ else
 	DB_ROOT_PASS='administrator'
 fi
 echo 'Shutting down any mysql processes...'
-sudo service mysql stop
-sudo killall -vw mysqld
-sudo mysqld_safe --skip-grant-tables >res 2>&1 &
+service mysql stop
+killall -vw mysqld
+mysqld_safe --skip-grant-tables >res 2>&1 &
 echo 'Reset MySQL services behaviour...'
 sleep 3
-sudo mysql -e "FLUSH PRIVILEGES;ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';FLUSH PRIVILEGES;"
+mysql -e "FLUSH PRIVILEGES;ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';FLUSH PRIVILEGES;"
 echo 'Cleaning up...'
-sudo killall -v mysqld
-sudo service mysql restart
-sudo rm res
+killall -v mysqld
+service mysql restart
+rm res
 echo -e "MySQL password has been modified to the default\nMySQL default root password: $DB_ROOT_PASS\nWriting to file: mysql_password.txt"
-sudo echo $DB_ROOT_PASS > /etc/jupyter/mysql_password.txt
+echo $DB_ROOT_PASS > /etc/jupyter/mysql_password.txt
