622955fdb3812f263c0b9a40a80c7daf07a13a52
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-20 21:33:21 +0700

    Update install_admin_scripts.sh

--- install_admin_scripts.sh
+++ install_admin_scripts.sh
@@ -12,17 +12,17 @@ echo
 if [[ "${CONFIRM}" =~ ^[Yy]$ ]]; then
 	echo -e -n "\nNote that you can only use the scripts normally using root user.\n"
 	# Creating folders in ~/admin-scripts
-	sudo mkdir ~/admin-scripts
-	sudo mkdir ~/admin-scripts/user-interface
-	sudo mkdir ~/admin-scripts/tutorials-notebooks
-	sudo mkdir ~/admin-scripts/jupyter-external-packages
+	mkdir ~/admin-scripts
+	mkdir ~/admin-scripts/user-interface
+	mkdir ~/admin-scripts/tutorials-notebooks
+	mkdir ~/admin-scripts/jupyter-external-packages
 
 	# Copy files and folers to the right place
-	sudo cp -TRv admin-scripts  ~/admin-scripts
-	sudo cp -TRv user-interface ~/admin-scripts/user-interface
-	sudo cp -TRv tutorials-notebooks ~/admin-scripts/tutorials-notebooks
-	sudo cp -TRv jupyter-ext-pkg ~/admin-scripts/jupyter-external-packages
-	sudo chmod +rwx ~/admin-scripts/*.sh
+	cp -TRv admin-scripts  ~/admin-scripts
+	cp -TRv user-interface ~/admin-scripts/user-interface
+	cp -TRv tutorials-notebooks ~/admin-scripts/tutorials-notebooks
+	cp -TRv jupyter-ext-pkg ~/admin-scripts/jupyter-external-packages
+	chmod +rwx ~/admin-scripts/*.sh
 
 	# Display finished text
 	echo -e -n "\nInstalled admin-scripts for active user."
