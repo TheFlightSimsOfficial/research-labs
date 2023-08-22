86bfd2ea3a5802bc0b9742afcb030f6289b899bf
つき 滝猫 <systakineko.tfs@gmail.com>
2023-08-22 19:03:37 +0700

    Remove dist-packages

--- jupyter-ext-pkg/ibm-q-lab/local/lib/python3.10/dist-packages/ibm_quantum_widgets/edit_circuit.py
+++
@@ -1,8 +0,0 @@
-from .circuit_composer import CircuitComposer
-from IPython.display import display
-
-def edit_circuit(circuit = None):
-  circuitComposer = CircuitComposer(circuit=circuit)
-  display(circuitComposer)
-
-  return circuitComposer
--- jupyter-ext-pkg/ibm-q-lab/local/lib/python3.10/dist-packages/ibm_quantum_widgets/utils.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Copyright (c) IBM Research.
-# Distributed under the terms of the Modified BSD License.
-
-DEFAULT_QREGS = 3
-DEFAULT_CREGS = 3
-DEFAULT_QASM = 'OPENQASM 2.0;include "qelib1.inc";qreg q[{}];creg c[{}];'.format(DEFAULT_QREGS, DEFAULT_CREGS)
